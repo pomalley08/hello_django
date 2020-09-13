b7067996afe708e86a3953c488b8873df0144f5a
Patrick O'Malley <pomalley08@users.noreply.github.com>
2020-09-13 06:21:05 -0400

    Extend a base template

--- hello/views.py
+++ hello/views.py
@@ -4,7 +4,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
 
 def hello_there(request, name):
     return render(
